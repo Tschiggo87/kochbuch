ec3c4cc62c7a01ec9c751a203c8e081af3708821
Enrico Kälin <97579163+Tschiggo87@users.noreply.github.com>
2023-06-23 08:43:22 +0200

    Change the name of the files "Hello" and HelloController to Main and MainController. Also change the Name from list-view to main view

--- src/main/java/com/example/kochbuch/Main.java
+++ src/main/java/com/example/kochbuch/Main.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("list-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1200, 700);
         scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm()); // CSS zur Scene hinzufügen
         stage.setTitle("Hello!");
