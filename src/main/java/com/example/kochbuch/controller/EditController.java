dbb6cc6e25231a2da52c3165bffde79864402b8e
Ari <arianit.rexhaj@hotmail.ch>
2023-07-02 11:33:33 +0200

    @edit-view @ editcontroller

---
+++ src/main/java/com/example/kochbuch/controller/EditController.java
@@ -0,0 +1,54 @@
+package com.example.kochbuch.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
+import com.example.kochbuch.model.RezeptModel;
+
+public class EditController {
+
+    public TextField name;
+    public TextField beschreibung;
+    public TextField dauer;
+    public TextField anzahlDerPortionen;
+    public TextField schwierigkeitsgrad;
+    public TextField anweisungen;
+    private RezeptModel model;
+
+    @FXML
+    private void initialize(){
+
+        model = new RezeptModel();
+        onShowValues();
+
+        bindModel();
+    }
+
+    private void bindModel(){
+        name.textProperty().bindBidirectional(model.nameProperty());
+        beschreibung.textProperty().bindBidirectional(model.beschreibungProperty());
+        dauer.textProperty().bindBidirectional(dauer.textProperty());
+        anzahlDerPortionen.textProperty().bindBidirectional(anzahlDerPortionen.textProperty());
+        schwierigkeitsgrad.textProperty().bindBidirectional(schwierigkeitsgrad.textProperty());
+        anweisungen.textProperty().bindBidirectional(anweisungen.textProperty());
+    }
+
+    public void onShowValues() {
+        System.out.println(model.toString());
+
+    }
+
+    public void onReset() {
+        model.setName(null);
+        model.setBeschreibung(null);
+        model.setDauer(0); // Setzen Sie den Standardwert für die Dauer auf 0
+        model.setAnzahlDerPortionen(0); // Setzen Sie den Standardwert für die AnzahlDerPortionen auf 0
+        model.setSchwierigkeitsgrad(null);
+        model.setAnweisungen(null);
+        //Durch das Setzen auf 0 geben Sie an, dass die Dauer und die Anzahl der Portionen auf ihren Standardwert zurückgesetzt werden sollen (bei Integer).
+
+    }
+
+}
