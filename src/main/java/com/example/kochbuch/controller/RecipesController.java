ac513205d2d89d5eda9702339f2c4319871defb1
Enrico Kälin <97579163+Tschiggo87@users.noreply.github.com>
2023-07-04 09:22:34 +0200

    Add interaction between recipes-view.fxml and recipes-detail-view.fxml

--- src/main/java/com/example/kochbuch/controller/RecipesController.java
+++ src/main/java/com/example/kochbuch/controller/RecipesController.java
@@ -40,6 +40,41 @@ public class RecipesController {
     private ImageView recipeImage5;
     @FXML
     private ImageView recipeImage6;
+    @FXML
+    private Label recipeDescription1;
+
+    @FXML
+    private Label recipeDescription2;
+
+    @FXML
+    private Label recipeDescription3;
+
+    @FXML
+    private Label recipeDescription4;
+
+    @FXML
+    private Label recipeDescription5;
+
+    @FXML
+    private Label recipeDescription6;
+
+    @FXML
+    private Label recipeDifficulty1;
+
+    @FXML
+    private Label recipeDifficulty2;
+
+    @FXML
+    private Label recipeDifficulty3;
+
+    @FXML
+    private Label recipeDifficulty4;
+
+    @FXML
+    private Label recipeDifficulty5;
+
+    @FXML
+    private Label recipeDifficulty6;
 
     private DatabaseHelper databaseHelper;
 
@@ -55,26 +90,39 @@ public class RecipesController {
         // Rezept 1
         recipeImage1.setImage(new Image("file:" + imageDirectory + rezepte.get(0).getBild()));
         recipeName1.setText(rezepte.get(0).getName());
+        recipeDescription1.setText(rezepte.get(0).getBeschreibung());
+        recipeDifficulty1.setText(rezepte.get(0).getSchwierigkeitsgrad());
+
 
         // Rezept 2
         recipeImage2.setImage(new Image("file:" + imageDirectory + rezepte.get(1).getBild()));
         recipeName2.setText(rezepte.get(1).getName());
+        recipeDescription2.setText(rezepte.get(1).getBeschreibung());
+        recipeDifficulty2.setText(rezepte.get(1).getSchwierigkeitsgrad());
 
         // Rezept 3
         recipeImage3.setImage(new Image("file:" + imageDirectory + rezepte.get(2).getBild()));
         recipeName3.setText(rezepte.get(2).getName());
+        recipeDescription3.setText(rezepte.get(2).getBeschreibung());
+        recipeDifficulty3.setText(rezepte.get(2).getSchwierigkeitsgrad());
 
         // Rezept 4
         recipeImage4.setImage(new Image("file:" + imageDirectory + rezepte.get(3).getBild()));
         recipeName4.setText(rezepte.get(3).getName());
+        recipeDescription4.setText(rezepte.get(3).getBeschreibung());
+        recipeDifficulty4.setText(rezepte.get(3).getSchwierigkeitsgrad());
 
         // Rezept 5
         recipeImage5.setImage(new Image("file:" + imageDirectory + rezepte.get(4).getBild()));
         recipeName5.setText(rezepte.get(4).getName());
+        recipeDescription5.setText(rezepte.get(4).getBeschreibung());
+        recipeDifficulty5.setText(rezepte.get(4).getSchwierigkeitsgrad());
 
         // Rezept 6
         recipeImage6.setImage(new Image("file:" + imageDirectory + rezepte.get(5).getBild()));
         recipeName6.setText(rezepte.get(5).getName());
+        recipeDescription6.setText(rezepte.get(5).getBeschreibung());
+        recipeDifficulty6.setText(rezepte.get(5).getSchwierigkeitsgrad());
     }
 
     @FXML
