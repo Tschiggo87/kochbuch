0870e21b37e4623e56ad81770bae3b14aa2da388
Ari <arianit.rexhaj@hotmail.ch>
2023-07-13 18:17:59 +0200

    random

--- src/main/java/com/example/kochbuch/controller/RecipesDetailController.java
+++ src/main/java/com/example/kochbuch/controller/RecipesDetailController.java
@@ -16,9 +16,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import com.example.kochbuch.model.RezeptModel;
-import com.example.kochbuch.controller.EditController;
-
 
 public class RecipesDetailController {
 
